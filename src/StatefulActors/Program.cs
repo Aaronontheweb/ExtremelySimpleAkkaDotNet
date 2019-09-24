3b0d6aab95cbfeb2169afc5c0ce033671f6ea059
Aaron Stannard <aaron@petabridge.com>
2019-09-24 02:26:14 -0500

    stateful actors

---
+++ src/StatefulActors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatefulActors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
